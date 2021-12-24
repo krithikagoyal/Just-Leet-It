d901e8a1de21fd722ee0ea2264d05e166a32fd0f
Krithika Goyal <56429808+krithikagoyal@users.noreply.github.com>
2021-12-24 15:20:03 +0530

    Time: 0 ms (100.00%), Space: 13.6 MB (66.15%) - LeetHub

--- generate-parentheses/generate-parentheses.cpp
+++ generate-parentheses/generate-parentheses.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
-    
-    vector<string> solve(int n, vector<vector<string>>& dp) {
-        if (n == 0 || dp[n].size() != 0)
-            return dp[n];
-        vector<string> ans, fans;
-        for (int i = 0; i < n; i++) {
-            vector<string> v1 = solve(i, dp);
-            vector<string> v2 = solve(n - i - 1, dp);
-            for (string& x : v1) {
-                ans.push_back('(' + x + ')');
-            }
-            for (string& x : v2) {
-                for (string& y : ans) {
-                    fans.push_back(y + x);
-                }
-            }
-            ans.clear();
-        }
-        return dp[n] = fans;
-    }
-    
     vector<string> generateParenthesis(int n) {
-        vector<vector<string>> dp(n + 1);
-        dp[1] = {"()"};
-        dp[0] = {""};
-        return solve(n, dp);
+        vector<string> ret;
+        helper(ret, "", n, n);
+        return ret;
+    }
+    void helper(vector<string> & res, string str, int left, int right){
+        if(left == 0 && right == 0){
+            res.push_back(str);
+            return;
+        }
+        if(left > 0) helper(res, str + "(", left - 1, right);
+        if(right > left) helper(res, str + ")", left, right - 1);
     }
 };
