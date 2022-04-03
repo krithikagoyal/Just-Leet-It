bc2346fedc2b62c4f25d289de307029179921bf4
Krithika Goyal <56429808+krithikagoyal@users.noreply.github.com>
2022-04-03 23:20:56 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (88.01%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -3,13 +3,26 @@ public:
     bool wordBreak(string s, vector<string>& wordDict) {
         sort(wordDict.begin(), wordDict.end());
         int n = s.size();
-        vector<bool> dp(n, false);
-        for (int i = 0; i < n; i++) {
-            dp[i] = binary_search(wordDict.begin(), wordDict.end(), s.substr(0, i + 1));
-            for (int j = i; (j > 0) && (dp[i] == false); j--) {
-                dp[i] = binary_search(wordDict.begin(), wordDict.end(), s.substr(j, i - j + 1)) & dp[j - 1]; 
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+        for(int i = 1; i <= s.size(); i++){
+            for(string str: wordDict){
+                if(str.size() <= i){
+                    if(dp[i - str.size()]){
+                        if(s.substr(i - str.size(), str.size()).compare(str) == 0){
+                            dp[i] = true;
+                            break;
+                        }
+                    }
+                }
             }
         }
-        return dp[n - 1];
+        // for (int i = 0; i < n; i++) {
+        //     dp[i] = binary_search(wordDict.begin(), wordDict.end(), s.substr(0, i + 1));
+        //     for (int j = i; (j > 0) && (dp[i] == false); j--) {
+        //         dp[i] = binary_search(wordDict.begin(), wordDict.end(), s.substr(j, i - j + 1)) & dp[j - 1]; 
+        //     }
+        // }
+        return dp[n];
     }
 };
