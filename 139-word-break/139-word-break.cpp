8dfeee00ac37b573ed64ea687c6fa8aa1168f428
Krithika Goyal <56429808+krithikagoyal@users.noreply.github.com>
2022-04-03 18:34:05 +0530

    Time: 23 ms (40.20%), Space: 10.8 MB (56.10%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -3,15 +3,13 @@ public:
     bool wordBreak(string s, vector<string>& wordDict) {
         sort(wordDict.begin(), wordDict.end());
         int n = s.size();
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        vector<bool> dp(n, false);
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i; j++) {
-                dp[j][j + i] = binary_search(wordDict.begin(), wordDict.end(), s.substr(j, i + 1));
-                for (int k = j; (k < j + i) && (dp[j][j + i] == false); k++) {
-                    dp[j][j + i] = dp[j][j + i] | (dp[j][k] & dp[k + 1][j + i]);
-                }
+            dp[i] = binary_search(wordDict.begin(), wordDict.end(), s.substr(0, i + 1));
+            for (int j = i; (j > 0) && (dp[i] == false); j--) {
+                dp[i] = binary_search(wordDict.begin(), wordDict.end(), s.substr(j, i - j + 1)) & dp[j - 1]; 
             }
         }
-        return dp[0][n - 1];
+        return dp[n - 1];
     }
 };
