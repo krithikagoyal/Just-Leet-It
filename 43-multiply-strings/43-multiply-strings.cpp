6d000d549d68f2f0f3ad62f73fb167fc4b57071f
Krithika Goyal <56429808+krithikagoyal@users.noreply.github.com>
2022-04-20 03:43:51 +0530

    Time: 31 ms (21.01%), Space: 16.5 MB (13.01%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -1,64 +1,86 @@
 class Solution {
-public:
-    string multiply(string num1, string num2) {
-        if (num1[0] == '0' || num2[0] == '0')
-            return "0";
-        string ans = "", to_add = "";
-        int carry = 0;
-        for (int i = num1.size() - 1; i >= 0; i--) {
-            for (int j = 0; j < num1.size() - i - 1; j++) {
-                ans += '0';
+private:
+    // Calculate the sum of all of the results from multiplyOneDigit.
+    string sumResults(vector<vector<int>>& results) {
+        // Initialize answer as a number from results.
+        vector<int> answer = results.back();
+        vector<int> newAnswer;
+        results.pop_back();
+        
+        // Sum each digit from answer and result
+        for (vector<int> result : results) {
+            newAnswer.clear();
+            int carry = 0;
+            
+            for (int i = 0; i < answer.size() || i < result.size(); ++i) {
+                // If answer is shorter than result or vice versa, use 0 as the current digit.
+                int digit1 = i < result.size() ? result[i] : 0;
+                int digit2 = i < answer.size() ? answer[i] : 0;
+                // Add current digits of both numbers.
+                int sum = digit1 + digit2 + carry;
+                // Set carry equal to the tens place digit of sum.
+                carry = sum / 10;
+                // Append the ones place digit of sum to answer.
+                newAnswer.push_back(sum % 10);
             }
-            for (int j = num2.size() - 1; j >= 0; j--) {
-                int val = (num1[i] - '0') * (num2[j] - '0') + carry;
-                carry = val / 10;
-                val %= 10;
-                char c = (char)(val + (int)'0');
-                ans += c;
+
+            if (carry) {
+                newAnswer.push_back(carry);
             }
-            while (carry) {
-                int val = carry % 10;
-                carry /= 10;
-                char c = char(val + '0');
-                ans += c;
-            }
-            int k = 0, l = 0;
-            string tmp = "";
-            while (k != to_add.size() && l != ans.size()) {
-                int val = (to_add[k] - '0') + (ans[l] - '0') + carry;
-                carry = val / 10;
-                val %= 10;
-                char c = char(val + '0');
-                tmp += c;
-                l++, k++;
-            }
-            while (k != to_add.size()) {
-                int val = (to_add[k] - '0') + carry;
-                carry = val / 10;
-                val %= 10;
-                char c = char(val + '0');
-                tmp += c;  
-                k++;
-            }
-            while (l != ans.size()) {
-                int val = (ans[l] - '0') + carry;
-                carry = val / 10;
-                val %= 10;
-                char c = (char)(val + (int)'0');
-                tmp += c;  
-                l++;
-            }
-            while (carry) {
-                int val = carry;
-                carry = val / 10;
-                val %= 10;
-                char c = char(val + '0');
-                tmp += c;  
-            }
-            to_add = tmp;
-            ans = "";
+            answer = newAnswer;
         }
-        reverse(to_add.begin(), to_add.end());
-        return to_add;
+        
+        // Convert answer to a string.
+        string finalAnswer;
+        for (int digit : answer) {
+            finalAnswer.push_back(digit + '0');
+        }
+        return finalAnswer;
+    }
+    
+    // Multiply the current digit of secondNumber with firstNumber.
+    vector<int> multiplyOneDigit(string& firstNumber, char& secondNumberDigit, int numZeros) {
+        // Insert zeros at the beginning based on the current digit's place.
+        vector<int> currentResult(numZeros, 0);
+        int carry = 0;
+
+        // Multiply firstNumber with the current digit of secondNumber.
+        for (char firstNumberDigit : firstNumber) {
+            int multiplication = (secondNumberDigit - '0') * (firstNumberDigit - '0') + carry;
+            // Set carry equal to the tens place digit of multiplication.
+            carry = multiplication / 10;
+            // Append last digit to the current result.
+            currentResult.push_back(multiplication % 10);
+        }
+
+        if (carry) {
+            currentResult.push_back(carry);
+        }
+        return currentResult;
+    }
+
+public:
+    string multiply(string firstNumber, string secondNumber) {
+        if (firstNumber == "0" || secondNumber == "0") {
+            return "0";
+        }
+        
+        // Reverse both numbers.
+        reverse(firstNumber.begin(), firstNumber.end());
+        reverse(secondNumber.begin(), secondNumber.end());
+        
+        // For each digit in secondNumber, multipy the digit by firstNumber and
+        // store the multiplication result (reversed) in results.
+        vector<vector<int>> results;
+        for (int i = 0; i < secondNumber.size(); ++i) {
+            results.push_back(multiplyOneDigit(firstNumber, secondNumber[i], i));
+        }
+        
+        // Add all the results in the results array, and store the sum in the answer string.
+        string answer = sumResults(results);
+        
+        // answer is reversed, so reverse it to get the final answer.
+        reverse(answer.begin(), answer.end());
+        return answer;
     }
 };
